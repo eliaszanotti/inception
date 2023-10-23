8a39f93d85f1bc248deb6028dcc45c9dda226535
Elias ZANOTTI <zanotti.elias@gmail.com>
2023-10-23 15:20:57 +0200

    fix mariadb script

--- srcs/requirements/mariadb/conf/mariadbConfiguration.sh
+++ srcs/requirements/mariadb/conf/mariadbConfiguration.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Terminate the script if an error occured
 set -e
 
-# Start MySQL
+echo -e "Start MySQL service..."
 service mysql start
+echo -e "\e[32m[MySQL service started]\e[0m"
 
 # Safe install of MySQL
-if [ ! -d "/var/lib/mysql/$WP_TITLE" ]
+if [ ! -d "/var/lib/mysql/$WP_TITLE" ]; then
+	echo "[First MySQL configuration]"
 
-then
-
-mysql_secure_installation << EOF
+	echo -e "mysql_secure_installation..."
+	mysql_secure_installation << EOF
 
 n
 y
@@ -19,18 +19,18 @@ y
 y
 y
 EOF
+	echo -e "\e[32m[MySQL Installed]\e[0m"
 
-# Generate the database and the user with privilege for WordPress
-mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $WP_TITLE;"
-mysql -uroot -e "CREATE USER IF NOT EXISTS '$WP_USER_LOGIN'@'%' IDENTIFIED BY '$WP_USER_PASSWORD';"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON $WP_TITLE.* TO '$WP_USER_LOGIN'@'%';"
-mysql -uroot -e "FLUSH PRIVILEGES;"
-mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-
+	echo -e "Creating databases..."
+	mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $WP_TITLE;"
+	mysql -uroot -e "CREATE USER IF NOT EXISTS '$WP_USER_LOGIN'@'%' IDENTIFIED BY '$WP_USER_PASSWORD';"
+	mysql -uroot -e "GRANT ALL PRIVILEGES ON $WP_TITLE.* TO '$WP_USER_LOGIN'@'%';"
+	mysql -uroot -e "FLUSH PRIVILEGES;"
+	mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+	echo -e "\e[32m[Databases created]\e[0m"
 fi
 
-# Shudown MySQL
+echo -e "\e[32m[Restarting MySQL]\e[0m"
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 
-# Execute
 exec "$@"
